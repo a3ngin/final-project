2f7f2843d682a63d570bc997434f06317aa77c7a
Anne Engin <anne@Annes-MacBook-Air.local>
2019-05-09 12:39:24 -0400

    building and adding static.json

---
+++ client/build/precache-manifest.1941c483a9e29d053670825f7be7688b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "56507d5b6a944da43f2c",
+    "url": "/static/css/main.581bb370.chunk.css"
+  },
+  {
+    "revision": "56507d5b6a944da43f2c",
+    "url": "/static/js/main.896ba55b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da6368ff76638df2db36",
+    "url": "/static/js/2.ffefb86e.chunk.js"
+  },
+  {
+    "revision": "b50021a7378d90009cc8efecc11da414",
+    "url": "/index.html"
+  }
+];
